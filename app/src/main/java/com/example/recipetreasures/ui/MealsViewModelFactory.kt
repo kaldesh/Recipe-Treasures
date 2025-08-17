e4356670eca4f2c2f98c4dadcbf4ee01711c4b00
zm237338 <zm237338@gmail.com>
2025-08-17 20:27:12 +0300

    MealsViewModel's Factory

---
+++ app/src/main/java/com/example/recipetreasures/ui/MealsViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.example.recipetreasures.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModelFactory(private val repo: MealsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MealsViewModel::class.java)) {
+            return MealsViewModel(repo) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
