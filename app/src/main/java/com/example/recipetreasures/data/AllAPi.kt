29a193695fd156124f5b0c499f4cf3acdb18706e
zm237338 <zm237338@gmail.com>
2025-08-17 09:33:47 +0300

    more of Ali's work: MainActivity: bottom navigation, about us, search, home

---
+++ app/src/main/java/com/example/recipetreasures/data/AllAPi.kt
@@ -0,0 +1,16 @@
+package com.example.recipetreasures.data
+
+import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object AllAPi {
+    const val BASE_URL="https://www.themealdb.com/api/json/v1/1/"
+    private val retrofit = Retrofit
+        .Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(Gson()))
+        .build()
+
+    val recipeEndPoint =retrofit.create(RecipeEndPoint::class.java)
+}
